c6bcae43db9cbd4b0e90304b72274e1f126393f2
Your Name <k2naysaa@gmail.com>
2026-07-14 13:27:40 +0800

    text

--- app/(admin)/admin/tickets/TicketsSearch.tsx
+++ app/(admin)/admin/tickets/TicketsSearch.tsx
@@ -19,7 +19,7 @@ export default function TicketsSearch({ groups }: { groups: TicketGroup[] }) {
     <Card>
       <CardHeader className="pb-3">
         <div className="flex items-center justify-between gap-4 flex-wrap">
-          <CardTitle className="text-base">Тасалбарын жагсаалт</CardTitle>
+          <CardTitle className="text-base">Сугалааны жагсаалт</CardTitle>
           <div className="relative w-full sm:w-64">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-400" />
             <Input
